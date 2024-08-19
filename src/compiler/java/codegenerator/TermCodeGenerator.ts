3ed9b291b6a2b4912fefc136c66f84cba684d434
martin-pabst <asv.pabst@gmail.com>
2024-08-19 16:02:06 +0200

    make methods of class Color more robust against nullpointerexception

--- src/compiler/java/codegenerator/TermCodeGenerator.ts
+++ src/compiler/java/codegenerator/TermCodeGenerator.ts
@@ -1138,6 +1138,7 @@ export abstract class TermCodeGenerator extends BinopCastCodeGenerator {
 
 
         let snippet = new SeveralParameterTemplate(objectTemplate).applyToSnippet(returnParameter, node.range, ...(<CodeSnippet[]>parameterValueSnippet));
+        snippet.finalValueIsOnStack = false;
 
         if (callingConvention == "java") {
             if (!snippet.endsWith(";\n")) snippet = new CodeSnippetContainer(SnippetFramer.frame(snippet, '§1;\n'));
