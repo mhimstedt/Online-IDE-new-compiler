9abe0be883e0f9c106128f04caee7f8eb85f7dbe
Johannes-Pabst <johannesmichael.pabst@gmail.com>
2024-08-28 16:02:13 +0200

    ...

--- src/compiler/java/runtime/graphics/3d/Material3dClass.ts
+++ src/compiler/java/runtime/graphics/3d/Material3dClass.ts
@@ -3,16 +3,32 @@ import * as THREE from "three"
 import { LibraryDeclarations } from "../../../module/libraries/DeclareType";
 import { NonPrimitiveType } from "../../../types/NonPrimitiveType";
 import { ObjectClass } from "../../system/javalang/ObjectClassStringClass";
+import { ColorClass } from "../ColorClass";
+import { ColorHelper } from "../../../lexer/ColorHelper";
 
 export class Material3dClass extends ObjectClass {
     static __javaDeclarations: LibraryDeclarations = [
-        { type: "declaration", signature: "class Material3d extends Object" }
+        { type: "declaration", signature: "class Material3d extends Object" },
+        { type: "method", signature: "void setColor(Color color)",native:Material3dClass.prototype.setColorColor},
+        { type: "method", signature: "void setColor(int color)",native:Material3dClass.prototype.setColorInt},
+        { type: "method", signature: "void setColor(String color)",native:Material3dClass.prototype.setColorString},
     ]
 
     static type: NonPrimitiveType;
 
-    material: THREE.Material;
+    material: THREE.MeshStandardMaterial;
 
-
+    setColorColor(color:ColorClass){
+        this.material.color.set(color.red,color.green,color.blue);
+    }
     
+    setColorInt(color: number){
+        this.material.color.set(color);
+    }
+    
+    setColorString(color: string){
+        let c = ColorHelper.parseColorToOpenGL(color);
+        this.material.color.set(c.color);
+    }
+
 }
--- src/compiler/java/runtime/graphics/3d/Mesh3dClass.ts
+++ src/compiler/java/runtime/graphics/3d/Mesh3dClass.ts
@@ -12,7 +12,7 @@ export class Mesh3dClass extends Object3dClass {
     static __javaDeclarations: LibraryDeclarations = [
         { type: "declaration", signature: "class Mesh3d extends Object3d", comment: JRC.Mesh3dClassComment },
         { type: "method", signature: "Mesh3d()", java: Mesh3dClass.prototype._cj$_constructor_$Mesh3d$ },
-        { type: "field", signature: "public Material3d material", template:"_material" },
+        { type: "field", signature: "public Material3d material"},
 
         { type: "method", signature: "void move(double x,double y,double z)"},
         { type: "method", signature: "final void move(Vector3 v)", native:Mesh3dClass.prototype.vmove},
@@ -34,7 +34,12 @@ export class Mesh3dClass extends Object3dClass {
         this.mesh.material = material.material;
     }
 
+    get material(): Material3dClass {
+        return this._material;
+    }
+
     _cj$_constructor_$Mesh3d$(t: Thread, callback: CallbackParameter){
+        this._material
         super._cj$_constructor_$Object3d$(t, callback);
     }    
 
