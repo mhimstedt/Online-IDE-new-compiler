f723e1eb1c5e08037767e47f01ec2a1773330b41
martin-pabst <asv.pabst@gmail.com>
2024-08-28 17:57:55 +0200

    ...

--- src/compiler/java/runtime/graphics/3d/CoordinateSystemHelper3d.ts
+++ src/compiler/java/runtime/graphics/3d/CoordinateSystemHelper3d.ts
@@ -19,7 +19,7 @@ export class CoordinateSystemHelper3d {
     axes: Axis[] = [
         {
             vector: new THREE.Vector3(1, 0, 0),
-            vector2: new THREE.Vector3(0, 1, 0),
+            vector2: new THREE.Vector3(0, -1, 0),
             color: 0xff0000,
             caption: "x",
             textureIndex: 0
@@ -33,7 +33,7 @@ export class CoordinateSystemHelper3d {
         },
         {
             vector: new THREE.Vector3(0, 0, 1),
-            vector2: new THREE.Vector3(1, 0, 0),
+            vector2: new THREE.Vector3(-1, 0, 0),
             color: 0x2020ff,
             caption: "z",
             textureIndex: 2
