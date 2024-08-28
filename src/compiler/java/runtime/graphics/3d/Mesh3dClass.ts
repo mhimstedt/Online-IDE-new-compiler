9e1916a197c7129c3fac267d8034dc8789f37a5b
martin-pabst <asv.pabst@gmail.com>
2024-08-28 15:53:05 +0200

    Object3d.scale works!

--- src/compiler/java/runtime/graphics/3d/Mesh3dClass.ts
+++ src/compiler/java/runtime/graphics/3d/Mesh3dClass.ts
@@ -29,6 +29,7 @@ export class Mesh3dClass extends Object3dClass {
         { type: "method", signature: "final void scaleY(double angleDeg)",native: Mesh3dClass.prototype.scaleY },
         { type: "method", signature: "final void scaleZ(double angleDeg)",native: Mesh3dClass.prototype.scaleZ },
         { type: "method", signature: "final void scale(Vector3 v)", native: Mesh3dClass.prototype.vscale },
+        { type: "method", signature: "final void scale(double d)", native: Mesh3dClass.prototype.scaleDouble },
 
     ];
 
