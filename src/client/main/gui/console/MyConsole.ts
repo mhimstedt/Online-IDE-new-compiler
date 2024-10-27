f528bb6b896b7ce6e7883b5883f01ded78c543e8
martin-pabst <asv.pabst@gmail.com>
2024-10-27 10:50:37 +0100

    bugfix: repl crashed when return value was void

--- src/client/main/gui/console/MyConsole.ts
+++ src/client/main/gui/console/MyConsole.ts
@@ -213,7 +213,7 @@ export class MyConsole {
 
     replReturnValueToOutput(replReturnValue: ReplReturnValue): string | undefined {
         if (typeof replReturnValue == "undefined") return undefined;
-        if (!replReturnValue.text) return undefined;
+        if(!replReturnValue.text) return undefined;
         let type: string = replReturnValue.type ? replReturnValue.type.toString() + " " : "";
         let text = replReturnValue.text;
         //@ts-ignore#
@@ -254,8 +254,8 @@ export class MyConsole {
         consoleTop.append(commandEntry.$consoleEntry);
 
         let replReturnOutputAsString = this.replReturnValueToOutput(value);
-        if (replReturnOutputAsString) {
-            let resultEntry = new ConsoleEntry(false, replReturnOutputAsString, value.value, null, null, true, color);
+        if(replReturnOutputAsString){
+            let resultEntry = new ConsoleEntry(false, replReturnOutputAsString, value.value,  null, null, true, color);
             this.consoleEntries.push(resultEntry);
             consoleTop.append(resultEntry.$consoleEntry);
         }
