e13dcdc499386ff4f6093b36e1f70c75d31c4b3d
Franz-Josef Färber <franzjoseffaerber@gmail.com>
2024-11-04 16:55:55 +0100

    CompilingProgressManager: formatting, private fields

--- src/compiler/java/CompilingProgressManager.ts
+++ src/compiler/java/CompilingProgressManager.ts
@@ -4,73 +4,73 @@ export class CompilingProgressManagerException {
 
 export class CompilingProgressManager {
 
-    private doLogging: boolean = false;
+    #doLogging: boolean = false;
 
-    private timeStarted: number = 0;
-    private lastInterruptionTime: number = 0;
-    private numberOfInterruptions: number = 0;
-    private interruptAndRestartFlag: boolean = false;
-    private newOrDirtyModules: string = "";
-    private _isInsideCompilationRun: boolean = false;
+    #timeStarted: number = 0;
+    #lastInterruptionTime: number = 0;
+    #numberOfInterruptions: number = 0;
+    #interruptAndRestartFlag: boolean = false;
+    #newOrDirtyModules: string = "";
+    #isInsideCompilationRun: boolean = false;
 
-    private interruptAtLeastEveryMs: number = 30;
-    private interruptForMs: 5;
+    #interruptAtLeastEveryMs: number = 30;
+    #interruptForMs: 5;
 
 
-    private nextRunWithoutInterruptions: boolean = false;
+    #nextRunWithoutInterruptions: boolean = false;
 
     get isInsideCompilationRun(): boolean {
-        return this._isInsideCompilationRun;
+        return this.#isInsideCompilationRun;
     }
 
-    initBeforeCompiling(){
-        this.timeStarted = this.lastInterruptionTime = performance.now();
-        this.interruptAndRestartFlag = false;
-        this.numberOfInterruptions = 0;
-        if(this.nextRunWithoutInterruptions) this.lastInterruptionTime += 1e6;
-        this.nextRunWithoutInterruptions = false;
-        this.newOrDirtyModules = "---";
-        this._isInsideCompilationRun = true;
+    initBeforeCompiling() {
+        this.#timeStarted = this.#lastInterruptionTime = performance.now();
+        this.#interruptAndRestartFlag = false;
+        this.#numberOfInterruptions = 0;
+        if (this.#nextRunWithoutInterruptions) this.#lastInterruptionTime += 1e6;
+        this.#nextRunWithoutInterruptions = false;
+        this.#newOrDirtyModules = "---";
+        this.#isInsideCompilationRun = true;
     }
 
-    afterCompiling(exception?: string){
-        this._isInsideCompilationRun = false;
-        let dt = performance.now() - this.timeStarted;
-        if(this.doLogging && this.newOrDirtyModules != '---'){
-            let withException: string = exception ? " with Exception " : "";
-            console.log("Compiled modules [" + this.newOrDirtyModules + "]" + withException + "; Compilation run took " + Math.round(dt) + " ms and was " + this.numberOfInterruptions + " times interrupted.");
+    afterCompiling(exception?: string) {
+        this.#isInsideCompilationRun = false;
+        const dt = performance.now() - this.#timeStarted;
+        if (this.#doLogging && this.#newOrDirtyModules != '---') {
+            const withException: string = exception ? " with Exception " : "";
+            console.log("Compiled modules [" + this.#newOrDirtyModules + "]" + withException + "; Compilation run took "
+                + Math.round(dt) + " ms and was " + this.#numberOfInterruptions + " times interrupted.");
         }
     }
 
     async interruptIfNeeded(): Promise<void> {
-        let dt = performance.now() - this.lastInterruptionTime;
-        if(dt >= this.interruptAtLeastEveryMs){
-            if(this.doLogging){
+        const dt = performance.now() - this.#lastInterruptionTime;
+        if (dt >= this.#interruptAtLeastEveryMs) {
+            if (this.#doLogging) {
                 console.log("Compiler was interrupted by CompilingProgressManger.");
             }
-            this.numberOfInterruptions++;
-            await this.setTimeout(this.interruptForMs);
-            this.lastInterruptionTime = performance.now();
-            if(this.interruptAndRestartFlag) throw new CompilingProgressManagerException();
+            this.#numberOfInterruptions++;
+            await this.#delay(this.#interruptForMs);
+            this.#lastInterruptionTime = performance.now();
+            if (this.#interruptAndRestartFlag) throw new CompilingProgressManagerException();
         }
     }
 
-    async setTimeout(ms: number): Promise<void> {
+    async #delay(ms: number): Promise<void> {
         return new Promise(resolve => setTimeout(resolve, ms));
     }
 
-    interruptCompilerIfRunning(nextRunWithoutInterruptions: boolean){
-        if(this.doLogging) console.log("Request to interrupt compiler by JavaCompletionItemProvider");
-        this.interruptAndRestartFlag = true;
-        this.nextRunWithoutInterruptions = nextRunWithoutInterruptions;
+    interruptCompilerIfRunning(nextRunWithoutInterruptions: boolean) {
+        if (this.#doLogging) console.log("Request to interrupt compiler by JavaCompletionItemProvider");
+        this.#interruptAndRestartFlag = true;
+        this.#nextRunWithoutInterruptions = nextRunWithoutInterruptions;
     }
 
-    restartNecessary(){
-        return this.interruptAndRestartFlag;
+    restartNecessary() {
+        return this.#interruptAndRestartFlag;
     }
 
-    setNewOrDirtyModules(newOrDirtyModules: string){
-        this.newOrDirtyModules = newOrDirtyModules;
+    setNewOrDirtyModules(newOrDirtyModules: string) {
+        this.#newOrDirtyModules = newOrDirtyModules;
     }
-
 }
