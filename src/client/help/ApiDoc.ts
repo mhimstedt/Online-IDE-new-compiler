c96de688a5b3e67d68ad7b1726ec4c00d5f41448
martin-pabst <asv.pabst@gmail.com>
2024-12-20 21:50:11 +0100

    little bugfix...

--- src/client/help/ApiDoc.ts
+++ src/client/help/ApiDoc.ts
@@ -33,7 +33,7 @@ export class ApiDoc {
 
     initEditor(){
 
-        JavaLanguage.registerLanguage();
+        JavaLanguage.getInstance();
 
         monaco.editor.defineTheme('myCustomThemeDark', {
             base: 'vs-dark', // can also be vs-dark or hc-black
