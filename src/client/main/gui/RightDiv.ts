ea67df331089e718f2a1f959a4d53a27226fc5a2
Martin Pabst <asv.pabst@gmail.com>
2025-04-09 15:31:30 +0200

    test child window

--- src/client/main/gui/RightDiv.ts
+++ src/client/main/gui/RightDiv.ts
@@ -4,6 +4,7 @@ import { MainBase } from "../MainBase.js";
 import { ClassDiagram } from "./diagrams/classdiagram/ClassDiagram.js";
 import { IWorld } from '../../../compiler/java/runtime/graphics/IWorld.js';
 import { SchedulerState } from "../../../compiler/common/interpreter/SchedulerState.js";
+import { DOM } from '../../../tools/DOM.js';
 
 export class RightDiv {
 
@@ -20,7 +21,7 @@ export class RightDiv {
 
         let withClassDiagram = this.$headings.find('.jo_classDiagramTabHeading').length > 0;
 
-        if(withClassDiagram){
+        if (withClassDiagram) {
             this.classDiagram = new ClassDiagram(this.$tabs.find('.jo_classdiagram'), main);
             this.$headings.find('.jo_classDiagramTabHeading').on("click", () => { that.main.drawClassDiagrams(false) });
         }
@@ -55,6 +56,38 @@ export class RightDiv {
             }
         });
 
+        // let child_window = window.open();
+
+        // const cdocument = child_window.document;
+        // const cbody = cdocument.body;
+        // cbody.style.margin = '0';
+        // cbody.style.padding = '0';
+
+        // let cssFence = DOM.makeDiv(cbody, 'joeCssFence');
+        // cssFence.appendChild($rightDiv[0]);
+        // cssFence.style.margin = '0';
+
+        // const rules = Array.from(document.styleSheets)
+        //     .reduce((sum, sheet) => {
+        //         // errors in CORS at some sheets (e.g. qiita)
+        //         // like: "Uncaught DOMException: Failed to read the 'cssRules' property from 'CSSStyleSheet': Cannot access rules"
+        //         try {
+        //             return [...sum, ...Array.from(sheet.cssRules).map(rule => rule.cssText)];
+        //         } catch (e) {
+        //             // console.log('errored', e);
+        //             return sum;
+        //         }
+        //     }, []).filter(rule => rule.indexOf('joeCssFence') >= 0)
+
+        // const newSheet = cdocument.querySelector('head').appendChild(document.createElement('style')).sheet;
+        
+        // // newSheet.insertRule('body{background-color: red}');
+        
+        // for(let rule of rules){
+        //     newSheet.insertRule(rule);
+        // }
+
+
     }
 
     adjustWidthToWorld() {
@@ -82,7 +115,7 @@ export class RightDiv {
 
     isClassDiagramEnabled(): boolean {
         let heading = this.$headings.find('.jo_classDiagramTabHeading');
-        if(heading.length == 0) return false;
+        if (heading.length == 0) return false;
         return heading.hasClass("jo_active");
     }
 
